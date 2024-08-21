f050db8e867cbe0f88aaff6034b337dfb188a63b
Bailey <84948636+bae12-jo@users.noreply.github.com>
2024-08-21 18:34:25 +0900

    Update userdata.sh

--- lib/ec2Stack/userdata.sh
+++ lib/ec2Stack/userdata.sh
@@ -31,7 +31,7 @@ After=network.target
 [Service]
 User=ubuntu
 Environment='AWS_DEFAULT_REGION=us-west-2'
-WorkingDirectory=/home/ubuntu/advanced-rag-workshop/application
+WorkingDirectory=/home/ubuntu/multi-modal-chatbot-with-advanced-rag/application
 ExecStartPre=/bin/bash -c 'sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8501'
 ExecStart=/bin/bash -c 'source /home/ubuntu/my_env/bin/activate && streamlit run streamlit.py --server.port 8501'
 Restart=always
